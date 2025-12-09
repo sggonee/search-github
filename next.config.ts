02c1eb0bef68508b21217e3157b96b730313c588
kevinGwon <developerGwon@gmail.com>
2025-12-09 16:03:05 +0900

    update next config for redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/github/search-users',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
