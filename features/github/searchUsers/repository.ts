885c530edf718a15e63bd215f446ffc516119cb4
kevinGwon <developerGwon@gmail.com>
2025-12-12 14:36:06 +0900

    refactor consolidate github user search logic into repository function

--- features/github/searchUsers/repository.ts
+++ features/github/searchUsers/repository.ts
@@ -1,10 +1,21 @@
-import { getGithubUsers } from '@/shared/github';
+import http from '@/shared/http';
 import type { GitHubSearchUsersRepository, GithubSearchUsers } from './interface';
 
 export function createGitHubSearchUserRepository(): GitHubSearchUsersRepository {
   return {
     async search(params: Record<string, string>): Promise<GithubSearchUsers> {
-      return await getGithubUsers(params);
+      return await http.get('https://api.github.com/search/users', {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28',
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        params: {
+          ...params,
+          per_page: '30',
+          order: 'desc',
+        },
+      });
     },
   };
 }
